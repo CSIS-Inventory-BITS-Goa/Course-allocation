41ff9d74a6ca72f1e8dae9060512e8912b2e4fd7
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2023-10-06 23:01:35 +0530

    :fire: Removed faculty-role clash

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost', // Update with your MySQL server host
     user: 'root', // Update with your MySQL username
-    password: 'password', // Update with your MySQL password
+    password: 'Password@123', // Update with your MySQL password
     database: 'testdb', // Update with your MySQL database name
 });
 
@@ -125,27 +125,28 @@ app.post("/allot-course", async (req, res) => {
             }
 
             // Step 2: Initialize variables to track allocated courses and roles
-            const allocatedCourses = new Set();
+            const allocatedFaculty = new Set();
             const allocatedRoles = new Set();
 
             // Step 3: Iterate through the sorted preferences and allocate courses
-            results.forEach(async (preferences) => {
+            const finalwrite =[]
+            const auth = await getAuthToken();
+            results.forEach((preferences) => {
                 const { faculty_id, course_code, preference, facultyrole, totalnoofcsstudnets, totalnootherdisciplinestudents } = preferences;
 
-                const auth = await getAuthToken();
-                const finalwrite =[]
+               
                 // Check if the role is already allocated for this course
-                if (!allocatedRoles.has(`${course_code}-${facultyrole}`)) {
+                if (!allocatedRoles.has(`${course_code}-${facultyrole}`) && !allocatedFaculty.has(`${faculty_id}-${facultyrole}`)) {
                     // Allocate the course to the faculty
                     console.log(`Allocating ${course_code} to ${faculty_id} with role ${facultyrole}`);
-                    allocatedCourses.add(course_code);
+                    allocatedFaculty.add(`${faculty_id}-${facultyrole}`);
                     allocatedRoles.add(`${course_code}-${facultyrole}`);
                     const data = [faculty_id,course_code, facultyrole, totalnoofcsstudnets, totalnootherdisciplinestudents];
                     finalwrite.push(data)
                 }
                 
-                appendData(auth, finalwrite);
             });
+            appendData(auth, finalwrite);
             res.status(201).json({ message: "Course allocation done" })
         })
     }
