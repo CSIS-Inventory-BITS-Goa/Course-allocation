acb8f14b6fab5228c32e0314e59cf17f38e1d53e
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2023-10-19 00:03:36 +0530

    extended add-faculty-preference and error truncation

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const mysql = require('mysql');
 
 const app = express();
 const port = 3000;
-const { getAuthToken, appendData } = require("./googleSheetsService.js");
+const {
+    getAuthToken,
+    appendData
+} = require("./googleSheetsService.js");
 // Multer middleware for file uploads
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({
+    storage
+});
 
 app.use(express.json());
-
 // MySQL database configuration
 const db = mysql.createConnection({
     host: 'localhost', // Update with your MySQL server host
@@ -44,6 +48,8 @@ db.on('error', (err) => {
     }
 });
 
+let programTypes = new Set(["OnCampus", "WILP"]);
+
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to CSIS API")
 });
@@ -53,10 +59,14 @@ app.post('/clear-course', (req, res) => {
     db.query('TRUNCATE TABLE course', (err, results) => {
         if (err) {
             console.error('Failed to clear COURSE table:', err);
-            res.status(500).json({ error: 'Failed to clear COURSE table.' });
+            res.status(500).json({
+                error: 'Failed to clear COURSE table.'
+            });
         } else {
             console.log('COURSE table cleared.');
-            res.json({ message: 'COURSE table cleared.' });
+            res.json({
+                message: 'COURSE table cleared.'
+            });
         }
     });
 });
@@ -71,43 +81,71 @@ app.post('/upload-course', upload.single('file'), (req, res) => {
                 throw err;
             }
 
-            console.log('DB has been cleared');
+            console.log('Database cleared');
 
             // Parse the Excel file from memory buffer
-            const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
+            const workbook = xlsx.read(req.file.buffer, {
+                type: 'buffer'
+            });
             const sheetName = workbook.SheetNames[0];
             const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
             // Insert data into the COURSE table
             const insertQueries = sheetData.map((row) => {
                 return new Promise((resolve, reject) => {
-                    db.query(
-                        'INSERT INTO COURSE (courseid, coursename, programtype, coursetype, semester) VALUES (?, ?, ?, ?, ?)',
-                        [row['COURSE NO'], row['COURSE TITLE'], row['ProgramType'], row['COURSETYPE'], row['SEMESTER']],
-                        (err, results) => {
-                            if (err) {
-                                console.error('Failed to insert data:', err);
-                                reject(err);
-                            } else {
-                                resolve(results);
+                    if (programTypes.has(row['ProgramType'])) {
+                        db.query(
+                            'INSERT INTO COURSE (courseid, coursename, programtype, coursetype, semester) VALUES (?, ?, ?, ?, ?)',
+                            [row['COURSE NO'], row['COURSE TITLE'], row['ProgramType'], row['COURSETYPE'], row['SEMESTER']],
+                            (err, results) => {
+                                if (err) {
+                                    console.error('Failed to insert data:', err);
+                                    reject(err);
+                                } else {
+                                    resolve(results);
+                                }
                             }
-                        }
-                    );
+                        );
+                    } else {
+
+                        reject(`data ${row['COURSE NO']}, ${row['COURSE TITLE']}, because of ${row['ProgramType']}`);
+                    }
                 });
             });
 
-            Promise.all(insertQueries)
-                .then(() => {
-                    res.json({ message: 'Data inserted into COURSE table.' });
-                })
-                .catch((error) => {
-                    console.error('Failed to insert data into COURSE table:', error);
-                    res.status(500).json({ error: 'Failed to insert data into COURSE table.' });
+
+            Promise.allSettled(insertQueries)
+                .then((results) => {
+                    const failedPromises = results.filter((result) => result.status === "rejected" && result.reason.substring(0, 4) === "data");
+                    if (failedPromises.length > 0) {
+                        failedPromises.forEach((promise) =>
+                            console.error('Failed to insert data into COURSE table:', promise.reason)
+                        );
+
+                        db.query('TRUNCATE TABLE COURSE ', async (err, results) => {
+                            if (err) {
+                                console.error('Failed to clear COURSE table:', err);
+                                throw err;
+                            }
+                        });
+                        console.log('Database cleared');
+                        res.status(500).json({
+                            error: 'Failed to insert data into COURSE table.'
+                        });
+                    } else {
+                        res.json({
+                            message: 'Data inserted into COURSE table.'
+                        });
+                    }
                 });
+
+
         });
     } catch (error) {
         console.error('Error:', error);
-        res.status(500).json({ error: 'Failed to insert data into COURSE table.' });
+        res.status(500).json({
+            error: 'Failed to insert data into COURSE table.'
+        });
     }
 });
 
@@ -121,7 +159,9 @@ app.post("/allot-course", async (req, res) => {
         db.query(query, async (err, results) => {
             if (err) {
                 console.error('Error querying the database:', err);
-                res.status(500).json({ error: err });;
+                res.status(500).json({
+                    error: err
+                });;
             }
 
             // Step 2: Initialize variables to track allocated courses and roles
@@ -129,50 +169,69 @@ app.post("/allot-course", async (req, res) => {
             const allocatedRoles = new Set();
 
             // Step 3: Iterate through the sorted preferences and allocate courses
-            const finalwrite =[]
+            const finalwrite = []
             const auth = await getAuthToken();
             results.forEach((preferences) => {
-                const { faculty_id, course_code, preference, facultyrole, totalnoofcsstudnets, totalnootherdisciplinestudents } = preferences;
+                const {
+                    faculty_id,
+                    course_code,
+                    preference,
+                    facultyrole,
+                    totalnoofcsstudnets,
+                    totalnootherdisciplinestudents
+                } = preferences;
 
-               
+
                 // Check if the role is already allocated for this course
                 if (!allocatedRoles.has(`${course_code}-${facultyrole}`) && !allocatedFaculty.has(`${faculty_id}-${facultyrole}`)) {
                     // Allocate the course to the faculty
                     console.log(`Allocating ${course_code} to ${faculty_id} with role ${facultyrole}`);
                     allocatedFaculty.add(`${faculty_id}-${facultyrole}`);
                     allocatedRoles.add(`${course_code}-${facultyrole}`);
-                    const data = [faculty_id,course_code, facultyrole, totalnoofcsstudnets, totalnootherdisciplinestudents];
+                    const data = [faculty_id, course_code, facultyrole, totalnoofcsstudnets, totalnootherdisciplinestudents];
                     finalwrite.push(data)
                 }
-                
+
             });
             appendData(auth, finalwrite);
-            res.status(201).json({ message: "Course allocation done" })
+            res.status(201).json({
+                message: "Course allocation done"
+            })
         })
-    }
-    catch (error) {
+    } catch (error) {
         console.error('Error:', error);
-        res.status(500).json({ error: 'Failed to create sample ALLOC statement' });
+        res.status(500).json({
+            error: 'Failed to create sample ALLOC statement'
+        });
     }
 })
 
 app.post('/add-faculty-preference', (req, res) => {
+
     const {
-        faculty_id,
+        course_id,
+        author_id,
         course_code,
         preference,
         facultyrole,
         totalnoofcsstudnets,
         totalnootherdisciplinestudents,
+        comments,
+        status,
+        recordtimestamp
     } = req.body;
+
+    let faculty_id = author_id;
 
     // Validate required fields
     if (!faculty_id || !course_code || !preference || !facultyrole) {
-        return res.status(400).json({ error: 'Missing required fields' });
+        return res.status(400).json({
+            error: 'Missing required fields'
+        });
     }
 
     // Insert new entry into FACULTYPREFERNCE table
-    const insertQuery = `
+    let insertQuery = `
         INSERT INTO FACULTYPREFERNCE (
             faculty_id,
             course_code,
@@ -183,7 +242,8 @@ app.post('/add-faculty-preference', (req, res) => {
         ) VALUES (?, ?, ?, ?, ?, ?)
     `;
 
-    const values = [
+
+    let values = [
         faculty_id,
         course_code,
         preference,
@@ -195,15 +255,54 @@ app.post('/add-faculty-preference', (req, res) => {
     db.query(insertQuery, values, (err, result) => {
         if (err) {
             console.error('Error inserting into FACULTYPREFERNCE:', err);
-            return res.status(500).json({ error: 'Failed to add faculty preference' });
+            return res.status(500).json({
+                error: 'Failed to add faculty preference'
+            });
         }
 
         console.log('New entry added to FACULTYPREFERNCE:', result.insertId);
-        res.status(201).json({ message: 'Faculty preference added successfully' });
+    });
+
+    insertQuery = `
+    INSERT INTO COURSEPREFERENCE(
+        courseid,
+        authorid,
+        facultyrole,
+        totalnoofcsstudnets,
+        totalnootherdisciplinestudents,
+        comments,
+        preferences,
+        status,
+        recordtimestamp
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    values = [
+        course_id,
+        author_id,
+        facultyrole,
+        totalnoofcsstudnets || null,
+        totalnootherdisciplinestudents || null,
+        comments || null,
+        preference,
+        status,
+        recordtimestamp
+    ];
+
+    db.query(insertQuery, values, (err, result) => {
+        if (err) {
+            console.error('Error inserting into COURSEPREFERENCE:', err);
+            return res.status(500).json({
+                error: 'Failed to add course preference'
+            });
+        }
+
+        console.log('New entry added to COURSEPREFERENCE:', result.insertId);
+        res.status(201).json({
+            message: 'Course preference added successfully'
+        });
     });
 });
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
