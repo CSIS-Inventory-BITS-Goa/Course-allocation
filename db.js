ddffdc400588e4b93012a5bbebfff5b6e0b0d2a4
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2023-11-21 23:16:30 +0530

    router and db config

---
+++ db.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql');
+// MySQL database configuration
+const db = mysql.createConnection({
+    multipleStatements: true,
+    host: 'localhost', // Update with your MySQL server host
+    user: 'root', // Update with your MySQL username
+    password: 'Password@123', // Update with your MySQL password
+    database: 'testdb', // Update with your MySQL database name
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL database:', err);
+    } else {
+        console.log('Connected to MySQL database');
+    }
+});
+
+// Handle MySQL disconnects
+db.on('error', (err) => {
+    console.error('MySQL database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        db.connect((err) => {
+            if (err)
+                console.error('Error reconnecting to MySQL database:', err);
+            else 
+                console.log('Reconnected to MySQL database');
+        });
+    } else console.error(err);
+});
+
+module.exports = { db };
---
+++ routers/taAllocation.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const { db } = require("../db");
+const taRouter = express.Router();
+
+
+module.exports = taRouter;
